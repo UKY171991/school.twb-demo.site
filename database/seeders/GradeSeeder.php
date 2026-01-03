9e3d74d521e894d17f6873a21e095c10f008f600
Umakant Yadav <37271073+UKY171991@users.noreply.github.com>
2026-01-03 17:34:25 +0530

    Update GradeSeeder.php

--- database/seeders/GradeSeeder.php
+++ database/seeders/GradeSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Grade;
+use App\Models\School;
 
 class GradeSeeder extends Seeder
 {
@@ -12,6 +14,32 @@ class GradeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Get the first school (or create one if none exists)
+        $school = School::first();
+        if (!$school) {
+            $school = School::create([
+                'name' => 'Default School',
+                'address' => 'Default Address',
+                'phone' => '0000000000',
+                'email' => 'default@school.com',
+            ]);
+        }
+
+        // Create sample grades
+        $grades = [
+            ['name' => 'Class 1', 'section' => 'A', 'capacity' => 40, 'grade_theme' => 1],
+            ['name' => 'Class 2', 'section' => 'A', 'capacity' => 35, 'grade_theme' => 2],
+            ['name' => 'Class 3', 'section' => 'B', 'capacity' => 45, 'grade_theme' => 3],
+            ['name' => 'Class 4', 'section' => 'A', 'capacity' => 40, 'grade_theme' => 4],
+            ['name' => 'Class 5', 'section' => 'B', 'capacity' => 38, 'grade_theme' => 5],
+        ];
+
+        foreach ($grades as $gradeData) {
+            Grade::create(array_merge($gradeData, [
+                'school_id' => $school->id,
+                'status' => 'active',
+                'description' => "Sample description for {$gradeData['name']}",
+            ]));
+        }
     }
 }
